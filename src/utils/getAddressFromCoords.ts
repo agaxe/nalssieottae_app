37feb94376e595ea0269a0707351eb3670c783ea
agaxe <agaxe.dev@gmail.com>
2023-03-21 09:59:55 +0900

    update: 현재 위치가 주소 서비스를 지원하지 않는 경우 예외 처리

--- src/utils/getAddressFromCoords.ts
+++ src/utils/getAddressFromCoords.ts
@@ -4,15 +4,30 @@ import type { Coords } from '@/shared/types/coords';
 export const getAddressFromCoords = async ({ latitude, longitude }: Coords) => {
   const url = `https://dapi.kakao.com/v2/local/geo/coord2regioncode.json?x=${longitude}&y=${latitude}`;
 
-  const address = await fetch(url, {
-    headers: {
-      Authorization: `KakaoAK ${KAKAO_API_KEY}`,
-    },
-  }).then(res => res.json());
+  try {
+    const response = await fetch(url, {
+      headers: {
+        Authorization: `KakaoAK ${KAKAO_API_KEY}`,
+      },
+    }).then(res => res.json());
 
-  if (!address.meta.total_count) {
-    return '[위치 정보 없음]';
+    //* error
+    if (response.code) {
+      throw new Error(response.code);
+    }
+
+    if (!response.meta.total_count) {
+      return '[위치 정보 없음]';
+    }
+
+    return response.documents[0].address_name;
+  } catch (error: unknown) {
+    if (!(error instanceof Error)) {
+      return;
+    }
+
+    if (error.message === '-2') {
+      return '[서비스 외 지역]';
+    }
   }
-
-  return address.documents[0].address_name;
 };
